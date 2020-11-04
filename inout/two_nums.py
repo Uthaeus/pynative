c71d3d59e237ceefa47ca0cd1de84628403986d6
Uthaeus <romanlavery@gmail.com>
2020-11-04 15:55:38 -0700

    two_nums

---
+++ inout/two_nums.py
@@ -0,0 +1,13 @@
+
+
+def two():
+    print("Enter first number: ")
+    a = input()
+    print("Enter second number: ")
+    b = input()
+
+    result = a + b 
+    print("Your sum is: {}".format(result))
+
+
+two()
