71eeb62d63ec283c47eabb75604dcd99b5e3cc7d
Uthaeus <romanlavery@gmail.com>
2020-11-07 11:17:27 -0700

    completed last.js

---
+++ strings/last.js
@@ -0,0 +1,10 @@
+
+
+const last = str => {
+    let e = 'Emma';
+    return str.lastIndexOf(e);
+}
+
+let str1 = "Emma is a data scientist who knows Python. Emma works at google.";
+
+console.log(last(str1));
