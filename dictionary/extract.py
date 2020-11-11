309af3201c49b0b7130b9afe458bf8d188d6f0ae
Uthaeus <romanlavery@gmail.com>
2020-11-11 13:58:37 -0700

    completed extract.py

---
+++ dictionary/extract.py
@@ -0,0 +1,15 @@
+
+
+sampleDict = {
+  "name": "Kelly",
+  "age":25,
+  "salary": 8000,
+  "city": "New york"
+  
+}
+
+keys = ['name', 'salary']
+
+myDict = {k: sampleDict[k] for k in keys}
+
+print(myDict)
