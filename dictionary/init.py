a8d7bacd929559758eb91f03034ea4a2546afed8
Uthaeus <romanlavery@gmail.com>
2020-11-11 13:46:15 -0700

    completed init.py

---
+++ dictionary/init.py
@@ -0,0 +1,10 @@
+
+
+employees = ['Kelly', 'Emma', 'John']
+defaults = {"designation": 'Application Developer', "salary": 8000}
+
+resDict = dict.fromkeys(employees, defaults)
+
+print(resDict)
+
+print(resDict['Kelly'])
