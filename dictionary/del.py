73d4becc366b6f21e6a97feb637d18e9d4a63532
Uthaeus <romanlavery@gmail.com>
2020-11-11 18:47:39 -0700

    completed del.py

---
+++ dictionary/del.py
@@ -0,0 +1,18 @@
+
+
+sampleDict = {
+  "name": "Kelly",
+  "age":25,
+  "salary": 8000,
+  "city": "New york"
+  
+}
+keysToRemove = ["name", "salary"]
+
+# sampleDict = {k: sampleDict[k] for k in sampleDict.keys() - keysToRemove}
+
+myDict = {}
+
+map(sampleDict.pop, keysToRemove)
+
+print(sampleDict)
