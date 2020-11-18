3b2a8b9bdb080e27cb48a69ca4386c3373d1eb16
Uthaeus <romanlavery@gmail.com>
2020-11-18 14:54:20 -0700

    working on bug for bus2.js

---
+++ oop/bus2.js
@@ -0,0 +1,29 @@
+
+
+class Vehicle {
+    constructor(name, max_speed, mileage) {
+        this.name = name;
+        this.max_speed = max_speed; 
+        this.mileage = mileage;
+    }
+
+    seating_capacity = (capacity) => {
+        return `The seating capacity of a ${this.name} is ${capacity} passengers`;
+    }
+}
+
+class Bus extends Vehicle {
+    constructor(name, max_speed, mileage, capacity = 50) {
+        super(name, max_speed, mileage);
+        this.capacity = capacity;
+    }
+
+    seating_capacity() {
+        super.seating_capacity(this.capacity);
+    }
+}
+
+
+let school_bus = new Bus('School Volvo', 180, 12);
+
+console.log(school_bus.seating_capacity());
